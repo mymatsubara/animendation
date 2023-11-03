100d4a9e3b00479a7b7c520d4d0658226168083e
Murilo Matsubara <murilo.matsubara@gmail.com>
2023-11-03 12:10:17 -0300

    feat: remove caching from turso db fetch

--- src/lib/server/db.ts
+++ src/lib/server/db.ts
@@ -1,11 +1,25 @@
 import { env } from '$env/dynamic/private';
 import type { DB } from '$lib/server/schema';
-import { LibsqlDialect } from '@libsql/kysely-libsql';
+// import { createClient } from '@libsql/client';
+import { LibsqlDialect, hrana } from '@libsql/kysely-libsql';
 import { Kysely } from 'kysely';
+
+const client = hrana.openHttp(
+	env.DATABASE_URL,
+	env.DATABASE_AUTH_TOKEN,
+	(input: string, init: any) => {
+		if (init?.cache) {
+			delete init.cache;
+			init.headers['Cache-Control'] = ' no-store, no-cache, must-revalidate, max-age=0';
+			init.headers['Pragma'] = 'no-cache';
+		}
+
+		return fetch(input, init);
+	}
+);
 
 export const db = new Kysely<DB>({
 	dialect: new LibsqlDialect({
-		url: env.DATABASE_URL,
-		authToken: env.DATABASE_AUTH_TOKEN
+		client
 	})
 });

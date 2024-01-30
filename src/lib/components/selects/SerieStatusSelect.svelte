571d9cf66151137449eabe712c04a2ce81db7d83
Murilo Matsubara <murilo.matsubara@gmail.com>
2024-01-29 21:50:35 -0300

    feat: add smart position to serie status select

--- src/lib/components/selects/SerieStatusSelect.svelte
+++ src/lib/components/selects/SerieStatusSelect.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
 	import { animeStatusDisplay, mangaStatusDisplay } from '$lib/client/display';
 	import type { AnimeStatus, MangaStatus } from '$lib/clients/myanimelist';
-	import CustomDropdown from '$lib/components/dropdowns/CustomDropdown.svelte';
 	import ChevronDownIcon from '$lib/components/icons/ChevronDownIcon.svelte';
 	import CloseIcon from '$lib/components/icons/CloseIcon.svelte';
 	import PlusIcon from '$lib/components/icons/PlusIcon.svelte';
@@ -9,12 +8,13 @@
 	import { toast } from '$lib/stores/toast';
 	import type { SerieType } from '$lib/types';
 	import { unchecked } from '$lib/utils/types';
-	import { Badge, Indicator } from 'flowbite-svelte';
+	import { Badge, Dropdown, Indicator } from 'flowbite-svelte';
 
 	export let serieId: number;
 	export let animelist: Myanimelist;
 	export let type: SerieType;
 
+	let open = false;
 	const displayMap = type === 'Anime' ? animeStatusDisplay : mangaStatusDisplay;
 	$: status =
 		type === 'Anime'
@@ -23,9 +23,7 @@
 
 	$: display = (displayMap as any)[status as string] ?? { label: 'Add to list', color: 'gray' };
 
-	async function changeStatus(newStatus: AnimeStatus | MangaStatus | undefined, toggle: Function) {
-		toggle();
-
+	async function changeStatus(newStatus: AnimeStatus | MangaStatus | undefined) {
 		if (status === newStatus) {
 			return;
 		}
@@ -42,41 +40,47 @@
 	}
 </script>
 
-<CustomDropdown class="-bottom-1 shadow-md" let:toggle>
-	<svelte:fragment slot="button" let:toggle>
-		<button class="w-max p-2 group" on:click={toggle}>
-			<Badge class="rounded-md px-2.5 py-0.5 group-focus:ring-2" color={display.color}>
-				{#if status}
-					<Indicator size="xs" class="mr-1" color={unchecked(display.color)} />{:else}
-					<PlusIcon stroke-width="2.0" class="h-3 mr-1" />
-				{/if}{display.label}
-				<ChevronDownIcon class="ml-1 h-3" /></Badge
-			>
-		</button>
-	</svelte:fragment>
+<div>
+	<button class="w-max p-2 group">
+		<Badge class="rounded-md px-2.5 py-0.5 group-focus:ring-2" color={display.color}>
+			{#if status}
+				<Indicator size="xs" class="mr-1" color={unchecked(display.color)} />{:else}
+				<PlusIcon stroke-width="2.0" class="h-3 mr-1" />
+			{/if}{display.label}
+			<ChevronDownIcon class="ml-1 h-3" /></Badge
+		>
+	</button>
 
-	<div class="flex flex-col w-max">
-		{#each Object.entries(displayMap) as [value, display] (value)}
-			{@const color = display.color === 'dark' ? 'gray' : display.color}
-			<button
-				class="flex items-center px-3 py-2 hover:bg-{color}-100 text-{color}-800 focus:ring-2 text-xs font-medium {status ===
-				value
-					? `bg-${color}-100`
-					: ''}"
-				on:click={() => changeStatus(unchecked(value), toggle)}
-			>
-				<Indicator size="xs" class="mr-2" color={unchecked(display.color)} />
-				{display.label}
-			</button>
-		{/each}
-		{#if status !== undefined}
-			<button
-				class="flex items-center px-3 py-2 hover:bg-red-100 text-red-800 text-xs font-medium"
-				on:click={() => changeStatus(undefined, toggle)}
-			>
-				<CloseIcon class="h-3 mr-2" />
-				Remove from list
-			</button>
-		{/if}
-	</div>
-</CustomDropdown>
+	<Dropdown class="border border-gray-300 rounded-lg" offset={0} placement="bottom-end" bind:open>
+		<div class="flex flex-col w-max">
+			{#each Object.entries(displayMap) as [value, display] (value)}
+				{@const color = display.color === 'dark' ? 'gray' : display.color}
+				<button
+					class="flex items-center px-3 py-2 hover:bg-{color}-100 text-{color}-800 focus:ring-2 text-xs font-medium {status ===
+					value
+						? `bg-${color}-100`
+						: ''}"
+					on:click={() => {
+						changeStatus(unchecked(value));
+						open = false;
+					}}
+				>
+					<Indicator size="xs" class="mr-2" color={unchecked(display.color)} />
+					{display.label}
+				</button>
+			{/each}
+			{#if status !== undefined}
+				<button
+					class="flex items-center px-3 py-2 hover:bg-red-100 text-red-800 text-xs font-medium"
+					on:click={() => {
+						changeStatus(undefined);
+						open = false;
+					}}
+				>
+					<CloseIcon class="h-3 mr-2" />
+					Remove from list
+				</button>
+			{/if}
+		</div>
+	</Dropdown>
+</div>
